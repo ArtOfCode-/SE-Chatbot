0d896f329dc99a6b41d155f111df8c000356d320
ArtOfCode- <owen.jenkins2903@gmail.com>
2015-07-02 16:08:12 +0100

    remove all, prep for migrate

--- Points_Data.p
+++
@@ -1,8 +0,0 @@
-(dp0
-Vzizouz212
-p1
-I1000
-sVartofcode
-p2
-L1000L
-s.
--- setup.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/ProgramFOX/SE-Chatbot.git
-cd SE-Chatbot
-git submodule init
-git submodule update
-sudo pip install beautifulsoup4
-sudo pip install requests --upgrade
-sudo pip install websocket-client --upgrade
